f5bb397292806957a363b01a04f51bb70f597d58
dev4us <bronxofsoul@gmail.com>
2020-01-06 00:01:12 +0900

    # BE 12. dev Update Resolver

---
+++ backend/src/api/User/UpdateUser/UpdateUser.js
@@ -0,0 +1,31 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { encryptPwd } from "../../../helpers/password";
+
+export default {
+  Mutation: {
+    UpdateUser: async (_, args, { request, verifyAuth }) => {
+      try {
+        verifyAuth(request);
+        const { nickname, password, profileImage } = args;
+        const { user } = request;
+
+        const encryptedPwd = await encryptPwd(password);
+
+        await prisma.updateUser({
+          data: { nickname, password: encryptedPwd, profileImage },
+          where: { id: user.id }
+        });
+
+        return {
+          ok: true,
+          error: null
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message
+        };
+      }
+    }
+  }
+};
