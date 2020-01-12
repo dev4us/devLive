2931f2d07804843ec8c60786e8754c1944b7b593
dev4us <bronxofsoul@gmail.com>
2020-01-12 23:26:45 +0900

    # BE 17. create GetLiveStreaming on Twitch

---
+++ backend/src/api/Twitch/GetLiveStreamsOnTwitch/GetLiveStreamsOnTwitch.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+export default {
+  Query: {
+    GetLiveStreamsOnTwitch: async (_, __) => {
+      try {
+        var headers = {
+          "Client-ID": process.env.TWITCH_API_KEY,
+          Accept: "application/vnd.twitchtv.v5+json"
+        };
+
+        const liveStreams = await fetch(
+          "https://api.twitch.tv/kraken/streams/?limit=5",
+          {
+            method: "GET",
+            headers: headers
+          }
+        ).then(res => res.json());
+
+        let liveStreamArr = [];
+
+        liveStreams.streams.map(val => {
+          liveStreamArr.push({
+            profileImg: val.channel.logo,
+            username: val.channel.name,
+            nickname: val.channel.display_name,
+            thumbnail: val.preview.medium
+          });
+        });
+
+        return {
+          ok: true,
+          error: null,
+          liveStreams: liveStreamArr
+        };
+      } catch (error) {
+        return { ok: false, error: error.message, liveStreams: null };
+      }
+    }
+  }
+};
